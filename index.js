afc283f2aa93cab00062345d2ebd6421f5f3beae
Sruthi <sruthivs64@gmail.com>
2023-12-21 13:39:37 +0530

    Fixed restart issue

--- index.js
+++ index.js
@@ -1,76 +1,91 @@
-// var Player={
-    
-//     turn:"X",
-//     create: function(values){
-//      var instance =Object.create(this);
-//      return instance;
-//     },
-//     changeTurns: function(sign){
-//      return this.sign==="X"?"O":"X";
-//     },
-// }
-
-// let cat=document.createElement('IMG');
-// cat.setAttribute('src', 'assets/cat-face.png');
-// var cat="background-image: url('https://img.icons8.com/doodle/144/satoru-gojo.png'); background-repeat: no-repeat; background-size: 120px 120px;background-position: center;" /*filter:hue-rotate(351deg) saturate(0.8) drop-shadow(1px 1px 4px black)"*/
-// var dog="background-image: url('assets/goku.png'); background-repeat: no-repeat; background-size: 120px 120px;background-position: center;" /*filter: hue-rotate(4deg) saturate(0.6);"*/
-
-var cat="background-image: url('https://img.icons8.com/doodle/144/satoru-gojo.png'); background-repeat: no-repeat; background-size: 120px 120px;background-position: center;" /*filter:hue-rotate(351deg) saturate(0.8) drop-shadow(1px 1px 4px black)"*/
-var dog="background-image: url('assets/goku.png'); background-repeat: no-repeat; background-size: 120px 120px;background-position: center;"
-
-// let dog=document.createElement('IMG');
-// dog.setAttribute('style', style);
-
+var gojo="background-image: url('https://img.icons8.com/doodle/144/satoru-gojo.png'); background-repeat: no-repeat; background-size: 120px 120px;background-position: center;" 
+var goku="background-image: url('assets/goku.png'); background-repeat: no-repeat; background-size: 130px 120px;background-position: center;"
 
 var Gameboard={
-    turn: cat,
-    data: "cat",
+  container: document.querySelector('.container'), 
+    turn: gojo,
+    data: "gojo",
+    arr: document.getElementsByClassName("box"),
+    gameOver: false,
     changeTurns(){
-    return (this.turn===cat?dog:cat);
+      return (this.turn===gojo?goku:gojo);
     },
     changeData(){
-    return (this.data==="cat"?"dog":"cat");
+    return (this.data==="gojo"?"goku":"gojo");
     },
-    arr: document.getElementsByClassName("box"),
-    winConditions:[
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],        
-        [2,4,6],   
-    ],
     checkWin(){
-            this.winConditions.forEach(i=>{
-          if( (this.arr[i[0]].getAttribute('data-name')===this.arr[i[1]].getAttribute('data-name')) && (this.arr[i[2]].getAttribute('data-name')===this.arr[i[1]].getAttribute('data-name')) && (this.arr[i[0]].getAttribute('data-name')!==null)){
-           console.log(this.arr[i[0]].getAttribute('data-name') + " Won");
+      let winConditions=[
+        [0, 1, 2, 0, 5, 0],
+        [3, 4, 5, 0, 15, 0],
+        [6, 7, 8, 0, 25, 0],
+        [0, 3, 6, -10, 15, 90],
+        [1, 4, 7, 0, 15, 90],
+        [2, 5, 8, 10, 15, 90],
+        [0, 4, 8, 0, 15, 45],
+        [2, 4, 6, 0, 15, 135],  
+    ];
+    let inner=document.getElementsByClassName("inner");
+          winConditions.forEach(i=>{
+          if( (inner[i[0]].getAttribute('data-name')===inner[i[1]].getAttribute('data-name')) && (inner[i[2]].getAttribute('data-name')===inner[i[1]].getAttribute('data-name')) && (inner[i[0]].getAttribute('data-name'))){
+            let winner=inner[i[0]].getAttribute('data-name');
+            console.log(winner + " Won");
+            document.querySelector('.gif').getElementsByClassName(winner)[0].style.width = "200px";
+            document.querySelector(".line").style.transform = `translate(${i[3]}vw, ${i[4]}vw) rotate(${i[5]}deg)`
+            document.querySelector(".line").style.width = "30vw";
+            this.reset(winner); 
+               
           }
         })
     },
-    playGame(){
+    playGame() {
+      console.log("Game starts");
+      
+      // Add click event listener to the container
+      document.querySelector('.container').addEventListener('click', (event) => {
+          const clickedElement = event.target.closest('.box .inner');
+          
+          if (clickedElement && !clickedElement.hasAttribute('data-name')) {
+              // If the clicked box is empty, proceed with the game logic
+              clickedElement.setAttribute('style', this.turn);
+              clickedElement.setAttribute('data-name', this.data);
+              
+              this.turn = this.changeTurns();
+              this.data = this.changeData();
+              
+              this.checkWin();
+          }
+      });
+  },
 
-        Array.from(this.arr).forEach(element => {
-       let boxtext = element.querySelector('.boxtext');
-       element.addEventListener("click", ()=>{
-       if(boxtext.innerText===""){
-       element.setAttribute('style', this.turn);
-       element.setAttribute('data-name', this.data);
-       this.turn= this.changeTurns();
-       this.data=this.changeData();
-       this.checkWin();
-       
-       }
-    }
-       )
-    });
-},
-    
-    
+reset(winner) {
+  let inner = document.querySelectorAll(".inner");
+  let resetid = document.getElementById("reset");
+  let reset = document.createElement('button');
+  reset.innerText = "Reset";
+  resetid.appendChild(reset);
+
+  reset.addEventListener('click', () => {
+      Array.from(inner).forEach(element => {
+          element.removeAttribute("style");
+          element.removeAttribute("data-name");
+      });
+
+      // Reset the line
+      document.querySelector(".line").style.transition = "width 0.5s ease-in-out";
+      document.querySelector(".line").style.width = "0px";
+      document.querySelector('.gif').getElementsByClassName(winner)[0].style.width = "0px";  
+
+      // Re-enable click handling
+      this.turn = gojo;
+      this.data = "gojo";
+      reset.style.display = "none";
+
+      // Re-add the click event listeners
+      this.playGame();
+  });
+}
+
 };
 
-var player=Object.create(Gameboard);
-player.playGame();
-
-
+var game=Object.create(Gameboard);
+game.playGame();
